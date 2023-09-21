a1d92feeeb5ae47602d2e52dd5e1bb85bee0ce7d
torikifukada <torikifukada@gmail.com>
2023-09-21 22:42:15 +0900

    many changes

--- src/futonScript.js
+++ src/futonScript.js
@@ -1,51 +1,69 @@
+let button = document.querySelector('#soumatou5 button');
+button.addEventListener('click', hideElements);
+
+function hideElements() {
+    for (let i = 0; i <= 5; i++) {
+        let element = document.getElementById('soumatou' + i);
+        element.style.display = 'none';
+    }
+}
+
 function takadafadeIn() {
-    let fadeDuration = 2000;  // フェードイン・フェードアウトの時間（ミリ秒）
-    let displayDuration = 4000;  // 透明度0の画像の表示時間（ミリ秒）
+    let fadeDuration = 3000;  // フェードイン・フェードアウトの時間（ミリ秒）
+    let waitAfterFade = 1500; // フェードイン後の待機時間（ミリ秒）
 
-    // 1. 最初のdiv要素をフェードインして表示
     let soumatou0 = document.getElementById('soumatou0');
-    soumatou0.style.transition = `opacity ${fadeDuration}ms`;
-    soumatou0.style.opacity = 1;
     soumatou0.style.display = 'block';
+    soumatou0.style.transition = `opacity ${fadeDuration}ms`;
 
-    setTimeout(function() {
-        // 2. soumatou1からsoumatou4までを表示
-        for(let i = 1; i <= 4; i++) {
-            setTimeout(function() {
+    setTimeout(function () {
+        soumatou0.style.opacity = 1;
+    }, 0);
+
+    setTimeout(function () {
+        for (let i = 1; i <= 4; i++) {
+            let element = document.getElementById(`soumatou${i}`);
+            element.style.display = 'block';
+        }
+        for (let i = 0; i <= 3; i++) {
+            setTimeout(function () {
                 let element = document.getElementById(`soumatou${i}`);
                 element.style.transition = `opacity ${fadeDuration}ms`;
-                element.style.opacity = 1;
-                element.style.display = 'block';
-            }, displayDuration * i);
-        }
+                if (i === 3) {
+                    let soumatou5 = document.getElementById('soumatou5');
+                    soumatou5.style.display = 'block';
+                    soumatou5.style.transition = `opacity 3000ms`;
 
-        setTimeout(function() {
-            // 3. 上からsoumatou3までを順番にフェードアウト
-            for(let i = 0; i <= 3; i++) {
-                setTimeout(function() {
-                    let element = document.getElementById(`soumatou${i}`);
-                    element.style.opacity = 0;
-                }, displayDuration * i);
-            }
+                    element.style.transition = `opacity 4000ms`;
 
-            setTimeout(function() {
-                // 4. soumatou5をフェードインして表示
-                let soumatou5 = document.getElementById('soumatou5');
-                soumatou5.style.transition = `opacity ${fadeDuration}ms`;
-                soumatou5.style.opacity = 1;
-                soumatou5.style.display = 'block';
-
-                // 5. buttonが押された場合soumatou1からsoumatou5を非表示にする
-                document.querySelector('#soumatou5 button').addEventListener('click', function() {
-                    for(let i = 1; i <= 5; i++) {
-                        let element = document.getElementById(`soumatou${i}`);
+                    setTimeout(function () {
                         element.style.opacity = 0;
-                        setTimeout(function() {
-                            element.style.display = 'none';
-                        }, fadeDuration);
-                    }
-                });
-            }, displayDuration * 4);
-        }, displayDuration * 4);
-    }, fadeDuration);
+                    }, 0);
+                    setTimeout(function () {
+                        setTimeout(function () {
+                            soumatou5.style.opacity = 1;
+                        }, 0);
+                        setTimeout(function () {
+                            let paragraph = document.querySelector('#soumatou5 p:nth-child(2)');
+                            paragraph.style.transition = `opacity 3000ms`;
+                            setTimeout(function () {
+                                paragraph.style.opacity = 1;
+                            }, 0);
+                            setTimeout(function () {
+                                paragraph.style.opacity = 1;
+                                    let button = document.querySelector('#soumatou5 button');
+                                    button.style.visibility = 'visible';
+                            }, 4000);
+                        }, 4000)
+                    }, 5500);
+                } else {
+                    setTimeout(function () {
+                        element.style.opacity = 0;
+                    }, 0);
+                }
+            }, (fadeDuration + waitAfterFade) * i);
+        }
+    }, fadeDuration + waitAfterFade);  // フェードイン時間と待機時間を合計
+    localStorage.setItem('getTakadaKenshi', 'true');
+    document.getElementsByTagName("title")[0].innerText = 'タカダゲーム';
 }
