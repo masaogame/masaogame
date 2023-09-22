4366a42fafe3ed543e15164d68f3979e5e642c11
torikifukada <torikifukada@gmail.com>
2023-09-22 23:34:51 +0900

    many changes.

--- src/futonScript.js
+++ src/futonScript.js
@@ -2,6 +2,9 @@ let button = document.querySelector('#soumatou5 button');
 button.addEventListener('click', hideElements);
 
 function hideElements() {
+    let blackout = document.getElementById('blackout');
+    blackout.style.display = 'none';
+
     for (let i = 0; i <= 5; i++) {
         let element = document.getElementById('soumatou' + i);
         element.style.display = 'none';
@@ -12,58 +15,69 @@ function takadafadeIn() {
     let fadeDuration = 3000;  // フェードイン・フェードアウトの時間（ミリ秒）
     let waitAfterFade = 1500; // フェードイン後の待機時間（ミリ秒）
 
-    let soumatou0 = document.getElementById('soumatou0');
-    soumatou0.style.display = 'block';
-    soumatou0.style.transition = `opacity ${fadeDuration}ms`;
+    let blackout = document.getElementById('blackout');
+    blackout.style.display = 'block';
+    blackout.style.transition = `opacity 1000ms`;
 
     setTimeout(function () {
-        soumatou0.style.opacity = 1;
+        blackout.style.opacity = 1;
     }, 0);
 
     setTimeout(function () {
-        for (let i = 1; i <= 4; i++) {
+        for (let i = 0; i <= 4; i++) {
             let element = document.getElementById(`soumatou${i}`);
             element.style.display = 'block';
         }
-        for (let i = 0; i <= 3; i++) {
+        setTimeout(function () {
+            blackout.style.transition = `opacity ${fadeDuration}ms`;
             setTimeout(function () {
-                let element = document.getElementById(`soumatou${i}`);
-                element.style.transition = `opacity ${fadeDuration}ms`;
-                if (i === 3) {
-                    let soumatou5 = document.getElementById('soumatou5');
-                    soumatou5.style.display = 'block';
-                    soumatou5.style.transition = `opacity 3000ms`;
+                blackout.style.opacity = 0;
+            }, 0);
+        }, 0)
+        setTimeout(function () {
+            for (let i = 0; i <= 3; i++) {
+                setTimeout(function () {
+                    let element = document.getElementById(`soumatou${i}`);
+                    element.style.transition = `opacity ${fadeDuration}ms`;
+                    if (i === 3) {
+                        let soumatou5 = document.getElementById('soumatou5');
+                        soumatou5.style.display = 'block';
+                        soumatou5.style.transition = `opacity 3000ms`;
 
-                    element.style.transition = `opacity 4000ms`;
+                        element.style.transition = `opacity 4000ms`;
 
-                    setTimeout(function () {
-                        element.style.opacity = 0;
-                    }, 0);
-                    setTimeout(function () {
                         setTimeout(function () {
-                            soumatou5.style.opacity = 1;
+                            element.style.opacity = 0;
                         }, 0);
                         setTimeout(function () {
-                            let paragraph = document.querySelector('#soumatou5 p:nth-child(2)');
-                            paragraph.style.transition = `opacity 3000ms`;
                             setTimeout(function () {
-                                paragraph.style.opacity = 1;
+                                soumatou5.style.opacity = 1;
                             }, 0);
                             setTimeout(function () {
-                                paragraph.style.opacity = 1;
+                                let paragraph = document.querySelector('#soumatou5 p:nth-child(2)');
+                                paragraph.style.transition = `opacity 3000ms`;
+                                setTimeout(function () {
+                                    paragraph.style.opacity = 1;
+                                }, 0);
+                                setTimeout(function () {
+                                    paragraph.style.opacity = 1;
                                     let button = document.querySelector('#soumatou5 button');
                                     button.style.visibility = 'visible';
-                            }, 4000);
-                        }, 4000)
-                    }, 5500);
-                } else {
-                    setTimeout(function () {
-                        element.style.opacity = 0;
-                    }, 0);
-                }
-            }, (fadeDuration + waitAfterFade) * i);
-        }
-    }, fadeDuration + waitAfterFade);  // フェードイン時間と待機時間を合計
+                                    let body = document.querySelector('body');
+                                    body.style.backgroundImage = "url('res/bg2-grad.webp')";
+                                }, 4000);
+                            }, 4000)
+                        }, 5500);
+                    } else {
+                        setTimeout(function () {
+                            element.style.opacity = 0;
+                        }, 0);
+                    }
+                }, (fadeDuration + waitAfterFade) * i);
+            }
+        }, fadeDuration)
+    }, 1000 + waitAfterFade);  // フェードイン時間と待機時間を合計
+
     localStorage.setItem('getTakadaKenshi', 'true');
     document.getElementsByTagName("title")[0].innerText = 'タカダゲーム';
 }
--- src/project.js
+++ src/project.js
@@ -3549,7 +3549,7 @@ window.__require = function e(t, n, o) {
                           a.default.score += 100, u.default.Instance.SetScoreTween(a.default.score), e.active = !1, a.default.playerTouch = !0, c.destroy()
                         }))), n.node.active = !1, t.node.active = !1, n.node.destroy(), t.node.destroy()
                       } else {
-                        s.addComponent(cc.Sprite).spriteFrame = l.default.Instance.fruit[reverseLevelUp ? 0 : 10], s.parent = c, s.position = cc.v2(0), d.default.Instance.Play(4, !1, 1), i.default.Instance.ribbonEffect(cc.v2(0, 0)), c.runAction(cc.sequence(cc.spawn(cc.jumpBy(1, 0, 0, 300, 1), cc.scaleTo(1, 1)), cc.delayTime(13), cc.spawn(cc.moveTo(1, cc.v2(0, 500)), cc.scaleTo(1, 0)), cc.callFunc(function () {
+                        s.addComponent(cc.Sprite).spriteFrame = l.default.Instance.fruit[reverseLevelUp ? 0 : 10], s.parent = c, s.position = cc.v2(0), d.default.Instance.Play(4, !1, 1), i.default.Instance.ribbonEffect(cc.v2(0, 0)), c.runAction(cc.sequence(cc.spawn(cc.jumpBy(1, 0, 0, 300, 1), cc.scaleTo(1, 1)), cc.delayTime(11.5), cc.spawn(cc.moveTo(1, cc.v2(0, 500)), cc.scaleTo(1, 0)), cc.callFunc(function () {
                           a.default.score += 100, u.default.Instance.SetScoreTween(a.default.score), takadafadeIn(), e.active = !1, a.default.playerTouch = !0, c.destroy()
                         }))), n.node.active = !1, t.node.active = !1, n.node.destroy(), t.node.destroy()
                       }
